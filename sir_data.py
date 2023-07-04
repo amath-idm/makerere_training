dfc61648d3583c48190b8d1ae130682b02ef3d66
cliffckerr <questionably@gmail.com>
2023-07-04 19:28:43 +0300

    add file for making data

---
+++ sir_data.py
@@ -0,0 +1,57 @@
+"""
+Make some data
+"""
+
+
+# Load packages
+import numpy as np
+import sciris as sc
+import pylab as pl
+
+sc.options(dpi=150)
+
+
+# Define our parameters
+beta = 0.5
+gamma = 0.2
+npts = 100
+I0 = 3
+N = 1000
+dt = 1
+
+for seed in range(5):
+    for randomize in [True, False]:
+        # seed = 1 
+        noise = 1.0
+        np.random.seed(seed)
+        
+        # Make arrays where we will store the estimates
+        x = np.arange(npts)
+        S = np.zeros(npts)
+        I = np.zeros(npts)
+        R = np.zeros(npts)
+        
+        # Initial conditions
+        S[0] = N - I0
+        I[0] = I0
+        
+        # Simulate the model over time
+        for t in x[:-1]:
+            infections = beta * S[t] * I[t]/N * dt
+            if randomize:
+                infections = np.random.poisson(infections) # Randomise it
+            recoveries = gamma * I[t] * dt
+        
+            S[t + 1] = S[t] - infections
+            I[t + 1] = I[t] + infections - recoveries
+            R[t + 1] = R[t] + recoveries
+        
+        # Plot the model estimate of the number of infections alongside the data
+        time = x * dt
+        if randomize:
+            pl.scatter(time, I, label='Data')
+        else:
+            pl.plot(time, I, label='Model', c='k')
+        pl.legend()
+        pl.show()
+    
--- sir_fit.py
+++ sir_fit.py
@@ -3,12 +3,10 @@ Fit an SIR model to data
 """
 
 
-# LOAD SOME PACKAGES
+# Load packages
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as pl
-from scipy.integrate import odeint
-import seaborn as sns
 
 # Read in the data and make a plot
 flu = pd.read_csv("influenza_cases.csv")
@@ -18,11 +16,11 @@ flu = pd.read_csv("influenza_cases.csv")
 # # pl.show()
 
 # Define our parameters
-beta = 3.5
-gamma = 1 / 5
-npts = 26
+beta = 0.5
+gamma = 0.2
+npts = 100
 I0 = 1
-N = 800
+N = 1000
 dt = 1
 
 # Make arrays where we will store the estimates
@@ -31,24 +29,23 @@ S = np.zeros(npts)
 I = np.zeros(npts)
 R = np.zeros(npts)
 
-# Initial conditions
+# Initial conditionsbeta * S[t] * I[t]/N
 S[0] = N - I0
 I[0] = I0
 
 # Simulate the model over time
 for t in x[:-1]:
-    dS = -beta * S[t] * I[t]/N
-    dI = beta * S[t] * I[t]/N - gamma * I[t]
-    dR = gamma * I[t]
+    infections = beta * S[t] * I[t]/N * dt
+    recoveries = gamma * I[t] * dt
 
-    S[t + 1] = S[t] + dS * dt
-    I[t + 1] = I[t] + dI * dt
-    R[t + 1] = R[t] + dR * dt
+    S[t + 1] = S[t] - infections
+    I[t + 1] = I[t] + infections - recoveries
+    R[t + 1] = R[t] + recoveries
 
 # Plot the model estimate of the number of infections alongside the data
 time = x * dt
 pl.plot(time, I, label='Model')
-pl.scatter(time, flu['cases'], label='Data')
+# pl.scatter(time, flu['cases'], label='Data')
 pl.legend()
 pl.show()
 
